3ede0f3d713bdc2c9a629ca53222e1f04259d32d
Jai Jemelie Mercado <mercadojaijemelie@gmail.com>
2026-04-28 23:55:26 +0800

    feat(engine/Move): add imports for model classes used in move representation

--- src/engine/Move.java
+++ src/engine/Move.java
@@ -1,4 +1,16 @@
 package engine;
 
+import model.Piece;
+import model.PieceType;
+import model.Square;
+
 public class Move {
+
+    private final Square from;
+    private final Square to;
+    private final Piece piece;
+    private final Piece capturedPiece;
+    private final MoveType type;
+    private final PieceType promotionType;
+
 }
