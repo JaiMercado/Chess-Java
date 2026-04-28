6cbfa96713a3ec22ac19fd84fbb798da2a686838
Raul Abanto <raulanthony.abanto@cit.edu>
2026-04-28 13:53:44 +0800

    feat(model): add opposite() method to Color enum

--- src/model/Color.java
+++ src/model/Color.java
@@ -1,4 +1,14 @@
 package model;
 
 public enum Color {
+    WHITE,
+    BLACK;
+
+    public Color opposite() {
+        if (this == WHITE) {
+            return BLACK;
+        } else {
+            return WHITE;
+        }
+    }
 }
