e91b7326380d5c06da8d853aaf3225ea0d5eb5a0
Jai Jemelie Mercado <mercadojaijemelie@gmail.com>
2026-04-28 23:45:15 +0800

    feat(engine): add GameStatus enum (ONGOING, CHECK, CHECKMATE, STALEMATE, DRAW, RESIGNED)

--- src/engine/GameStatus.java
+++ src/engine/GameStatus.java
@@ -1,4 +1,11 @@
 package engine;
 
+//Diri ky all the possible states the game can be in.
 public enum GameStatus {
+    ONGOING,
+    CHECK,
+    CHECKMATE,
+    STALEMATE,
+    DRAW,
+    RESIGNED
 }
